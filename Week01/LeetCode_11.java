05bf44ea7870733c8bc8ef6104e4ac952f9a1da0
Jimmy chen <jimmychen.ca@gmail.com>
2020-06-09 12:31:27 -0400

    Array - LeetCode-11

---
+++ Week01/LeetCode_11.java
@@ -0,0 +1,24 @@
+class Solution {
+    //11. 盛最多水的容器
+    public int maxArea(int[] height) {
+
+        int len = height.length;
+        int i=0, j=len-1,h=0;
+        int maxArea =0;
+
+        while (i<j) {
+            int area = Math.min(height[i],height[j])*(j-i);
+            maxArea = Math.max(area,maxArea);
+            //only need to move the index in lower side
+            if(height[i]<height[j]){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+
+        return maxArea;
+
+    }
+}
