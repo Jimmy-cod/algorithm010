159e75534e89cc7722bec073e96b10f1bacb80da
Jimmy chen <jimmychen.ca@gmail.com>
2020-06-10 11:41:59 -0400

    Week01: 283. 移动零

--- Week01/LC_11_MoveZeroes.java
+++ Week01/LC_11_MoveZeroes.java
@@ -1,16 +1,17 @@
-
-public void moveZeroes(int[] nums) {
-    //把0用后面的数代替，并统计0的个数记录在j上
-    int j =0;
-    for(int i=0;i<nums.length;i++){
-        if(nums[i]!=0){
-            nums[j] = nums[i];
+class Solution {
+    public void moveZeroes(int[] nums) {
+        //把0用后面的数代替，并统计0的个数记录在j上
+        int j =0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        //补上0
+        while(j<nums.length){
+            nums[j]=0;
             j++;
         }
-    }
-    //补上0
-    while(j<nums.length){
-        nums[j]=0;
-        j++;
     }
 }
