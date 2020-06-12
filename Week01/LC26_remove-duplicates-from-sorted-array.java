f03374fef9ee570a661198ed79dcc67dc5d86393
Jimmy chen <jimmychen.ca@gmail.com>
2020-06-11 22:03:14 -0400

    Week01 26. 删除排序数组中的重复项

--- Week01/LC26_remove-duplicates-from-sorted-array.java
+++ Week01/LC26_remove-duplicates-from-sorted-array.java
@@ -2,6 +2,9 @@
 class Solution {
     //代替法，用j保存修改数组的位置
     public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length==0){
+            return 0;
+        }
         int j =0;
         for(int i=0;i<nums.length;i++){
             if( nums[i] != nums[j]){
