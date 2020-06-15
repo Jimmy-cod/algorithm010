213f7b11db773a28cf69434aab4d64174ddb24c1
Jimmy chen <jimmychen.ca@gmail.com>
2020-06-15 00:37:17 -0400

    Week01 641. Design Circular Deque

---
+++ Week01/LC_641-design-circular-deque.java
@@ -0,0 +1,86 @@
+class MyCircularDeque {
+    int[] arr;
+    int front;
+    int real;
+    int capacity;
+    int size; //add size to track the side of deque
+
+
+    /** Initialize your data structure here. Set the size of the deque to be k. */
+    public MyCircularDeque(int k) {
+        capacity = k;
+        size = 0;
+        front = real =0;
+        arr = new int[capacity];
+    }
+
+    /** Adds an item at the front of Deque. Return true if the operation is successful. */
+    public boolean insertFront(int value) {
+        if (isFull()){
+            return false;
+        }
+        front = (front - 1 + capacity) % capacity;
+        arr[front] = value;
+        size++;
+        return true;
+    }
+
+    /** Adds an item at the rear of Deque. Return true if the operation is successful. */
+    public boolean insertLast(int value) {
+        if (isFull()){
+            return false;
+        }
+        arr[real] = value;
+        real = (real + 1) % capacity;
+        size++;
+        return true;
+    }
+
+    /** Deletes an item from the front of Deque. Return true if the operation is successful. */
+    public boolean deleteFront() {
+        if (isEmpty()){
+            return false;
+        }
+        front = (front + 1) % capacity;
+        size--;
+        return true;
+    }
+
+    /** Deletes an item from the rear of Deque. Return true if the operation is successful. */
+    public boolean deleteLast() {
+        if (isEmpty()){
+            return false;
+        }
+
+        real = (real - 1 + capacity) % capacity;
+        size--;
+        return true;
+    }
+
+    /** Get the front item from the deque. */
+    public int getFront() {
+        if (isEmpty()){
+            return -1;
+        }
+        return arr[front];
+    }
+
+    /** Get the last item from the deque. */
+    public int getRear() {
+        if (isEmpty()){
+            return -1;
+        }
+
+        return arr[(real-1+capacity) % capacity];
+    }
+
+    /** Checks whether the circular deque is empty or not. */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /** Checks whether the circular deque is full or not. */
+    public boolean isFull() {
+        return capacity == size;
+    }
+}
