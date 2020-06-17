0a48737253f5d7f38f8ba7c56f44d7cf6d3040fb
Jimmy chen <jimmychen.ca@gmail.com>
2020-06-17 14:33:46 -0400

    Week02 264. 丑数 II

--- Week02/LC_264_ugly_number_ii.java
+++ Week02/LC_264_ugly_number_ii.java
@@ -4,14 +4,18 @@ import java.util.HashMap;
 
 public class LC_264_ugly_number_ii {
 
-    HashMap
     public int nthUglyNumber(int n) {
         if (n<=0) return 0;
         int[] ugly = new int[n];
         ugly[0]=1;
-        int index2=0,index3=0,index5=0,i =1;
+        int index1=0,index2=0,index3=0,i =1;
         while(i<n){
-            ugly[i] = Math.min(ugly[i]*2,Math.min(ugly[i]*3,ugly[i]*5));
+            //找到最小的丑数，存在ugly[i]
+            ugly[i] = Math.min(ugly[index1]*2,Math.min(ugly[index2]*3,ugly[index3]*5));
+            //倒回去找相应的下标存起来
+            while(ugly[index1]*2==ugly[i]) index1++;
+            while(ugly[index2]*3==ugly[i]) index2++;
+            while(ugly[index3]*5==ugly[i]) index3++;
             i++;
         }
         return ugly[n-1];
